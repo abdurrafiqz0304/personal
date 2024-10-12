0ed9ebafb752be2e681a24b26a991cbdd696a193
Abdurrafiq Bin Zakaria <abdurrafiq@graduate.utm.my>
2024-10-13 07:08:31 +0800

    Add files via upload

---
+++ personal/1-3/Q3/discount.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float pr, npr;
+
+    cout << "Enter the price: ";
+    cin >> pr;
+
+    if (pr > 10){
+
+        npr = pr*0.9;
+        cout << "Total price is: ";
+        cout << npr << endl;
+                }
+        else {
+            pr = pr;
+            cout << "Total price is: ";
+            cout << pr << endl;
+        }
+        
+system("pause");
+
+    }
