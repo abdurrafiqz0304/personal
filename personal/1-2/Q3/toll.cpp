0b1dfcbfd9c0aee5a617f5fdcd6004e898666c9a
Abdurrafiq Bin Zakaria <abdurrafiq@graduate.utm.my>
2024-10-13 08:30:36 +0800

    Add files via upload

---
+++ personal/1-2/Q3/toll.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//Find the better solution please...
+//This solution looks like nonsense
+// 1. How to put hours in 24 format (i got octal error)
+
+
+int main(){
+
+    double km, h, rate;
+
+    cout << "Enter the expected distance in km: ";
+    cin >> km;
+    cout << endl << "Enter at what time you will at toll in (12 hours format): ";
+    cin >> h;
+
+    if (h >= 8 || h <= 9){
+
+        rate = 4 * km;
+        cout << "Total toll fee: RM";
+        cout << rate << endl;
+
+    } else {
+
+        rate = 3 * km;
+        cout << "Total toll fee: RM";
+        cout << rate << endl;
+
+    }
+
+    system("pause");
+
+}
