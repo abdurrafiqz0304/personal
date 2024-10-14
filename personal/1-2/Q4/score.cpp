241f7baecfc1d7580f468774affc77c2b8e02edd
Abdurrafiq Bin Zakaria <abdurrafiq@graduate.utm.my>
2024-10-14 14:00:56 +0800

    Add files via upload

---
+++ personal/1-2/Q4/score.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+//pening aa
+
+int main(){
+
+    double p, s, l, c;
+    s = 101;
+    l = -1;
+    c = 1;
+
+    cout << "Enter the percentage score: ";
+    cin >> p;
+    cout << endl;
+
+    if (p < s) {
+        s = p;
+    } else {
+        p > l;
+    }
+
+    if (p > l){
+        l = p;
+    } else {
+        c = c + 1;
+    }
+
+}
